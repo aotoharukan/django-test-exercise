c9ba9d6d83e8d5d49ffefb3b494d97b803d480ba
aotoharukan <s1f102302775@iniad.org>
2025-07-09 00:46:49 +0900

    確認4

--- todo/views.py
+++ todo/views.py
@@ -11,7 +11,7 @@ def index(request):
 
     tasks = Task.objects.all()
 
-    context = {'task': tasks}
+    context = {'tasks': tasks}
     return render(request, 'todo/index.html', context)
 
 # Create your views here.
